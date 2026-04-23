590b8236cd8fbd8be245d22bd4cb1a4c37bcdecf
Visha242629 <101812736+Visha242629@users.noreply.github.com>
2026-04-23 21:41:52 +0530

    Creation Startn of dashboard and SignUp Screen

--- lib/worker/deshboard.dart
+++ lib/worker/deshboard.dart
@@ -1,1 +1,10 @@
 import 'package:flutter/material.dart';
+
+class WorkerDashboard extends StatelessWidget {
+  const WorkerDashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text("This is dashboard")));
+  }
+}
