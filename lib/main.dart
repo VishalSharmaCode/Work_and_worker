06c6c085d83b9a9093ab6895187afdea707903f9
Visha242629 <101812736+Visha242629@users.noreply.github.com>
2026-04-26 21:46:03 +0530

    signup page of work provider

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'Splash/splash.dart'; // Import the SplashScreen widget
 import 'role_selection/role.dart'; // Import the RoleSelection widget
 import 'worker/login.dart'; // Import Login Screen of Worker
-import 'worker/Signup.dart';
-import 'practice_test/p_test.dart';
-import 'worker/deshboard.dart';
+import 'worker/Signup.dart'; // Import Login Screen of Worker
+import 'practice_test/p_test.dart'; //Import test Screen of Worker
+import 'worker/deshboard.dart'; //Import deshboard Screen of Worker
+import 'work_provider/login_wp.dart'; //Import login Screen of work provider
+import 'work_provider/signup_wp.dart'; // Import signup Screen of work provider
 
 void main() {
   runApp(const MyApp());
@@ -18,12 +20,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+
       // home: SplashScreen(), //Test SplashScreen
       // home: RoleSelection(), // Test Roleselection Screen
       // home: LoginWorker(), // Test Worker's Login Screen
       // home: SignUp_Worker(), // Test worker's Signup Screen
       // home: Test_Text_Button(),
-      home: WorkerDashboard(),
+      // home: WorkerDashboard(),
+      home: SignUp_Wp(),
     );
   }
 }
